700f23a494d7abd566f28abf6aed7ec948c8d77e
Samuel Loup <samuel.loup@gmail.com>
2014-10-15 11:07:23 +0200

    fix and simplify new binary commands handling

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 'use strict';
 /* Xcraft core bin shell extensions */
 
-module.exports = function() {
-  var route;
-  var settings = {};
+module.exports = function (settings) {
+  var app = settings.shell;
 
-  route = function(req, res, next) {
-    var app;
-    app = req.shell;
-
-    if (settings.workspace === null) {
-      settings.workspace = app.set('workspace');
-    }
-    if (settings.config === null) {
-      settings.config = '';
-    }
-
-    app.cmd('.node', 'launch nodejs', function(req, res, next) {
-      res.cyan('todo: launch .node').ln();
-      return res.prompt();
-    });
-
-    return next();
-  };
-  if (arguments.length === 1) {
-    settings = arguments[0];
-    return route;
-  } else {
-    return route.apply(null, arguments);
-  }
+  app.cmd('.node', 'launch nodejs', function (req, res, next) {
+    res.cyan('todo: launch .node').ln();
+    return res.prompt();
+  });
 };
