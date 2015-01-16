589fe401b5d657b413126998c54d8449de62a2ef
Mathieu Schroeter <schroeter@epsitec.ch>
2015-01-16 08:48:11 +0100

    Cosmetics.

--- $.js
+++ $.js
@@ -3,18 +3,19 @@
 
 var path = require ('path');
 var fs   = require ('fs');
-var xFs  = require ('xcraft-core-fs');
-var xUtils = require ('xcraft-core-utils');
 
-var busClient    = require ('xcraft-core-busclient');
+var xFs       = require ('xcraft-core-fs');
+var xUtils    = require ('xcraft-core-utils');
+var busClient = require ('xcraft-core-busclient');
+
 var cmd = {};
-var rc = {};
+var rc  = {};
 
 /**
-* Retrieve the list of available commands.
-*
-* @returns {Object} The list and definitions of commands.
-*/
+ * Retrieve the list of available commands.
+ *
+ * @returns {Object} The list and definitions of commands.
+ */
 exports.xcraftCommands = function () {
   var paths = process.env.PATH.split (path.delimiter);
   paths.forEach (function (location) {
@@ -22,25 +23,29 @@ exports.xcraftCommands = function () {
     if (!fs.existsSync (location)) {
       return;
     }
+
     var bins = xFs.ls (location, /\.(exe|bat|cmd)$/);
     bins.forEach (function (bin) {
       rc[bin] = {
         desc: 'execute ' + bin,
-        options : {
+        options: {
           scope: 'exec',
-          params : {
-            optional : 'argv...'
+          params: {
+            optional: 'argv...'
           }
         }
       };
+
       cmd[bin] = function (argv) {
         console.log (bin + ' : ' + argv);
         busClient.events.send ('$.' + bin + '.finished');
       };
     });
   });
+
   var destFile = path.join (__dirname, './rc.json');
   xUtils.json2JsonFile (rc, destFile);
+
   return {
     handlers: cmd,
     rc: destFile
